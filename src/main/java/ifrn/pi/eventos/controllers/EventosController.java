d4ac3171f976fe8cb893aa5da7097a5301efd89e
Larissa073 <90067657+Larissa073@users.noreply.github.com>
2021-11-17 18:43:59 -0300

    Update EventosController.java

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,7 +1,5 @@
 package ifrn.pi.eventos.controllers;
 
-import javax.persistence.Entity;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,32 +16,16 @@ public class EventosController {
 	
 	@RequestMapping("/eventos/form")
 	public String form() {
-		return "formEvento";
+		return "eventos/formEvento";
 	}
-	//@PostMapping("/eventos")//
-	//public String test(String nome,String local,String data,String horario) {
-		//System.out.println("O nome: "+ nome);
-		//System.out.println("O local: "+ local);
-		//System.out.println("A data: "+ data);
-		//System.out.println("O horario: "+ horario);
-		//aqui foi pego um metodo de cada vez e foi mostrado no console o que foi digitado na pagina.//
-		//return "evento-test";
-	//
 	
-	//@PostMapping("/eventos")
-	//public String test(Evento evento) {
-	//	System.out.println(evento.getNome());
-	//	System.out.println(evento.getLocal());
-	//	System.out.println(evento.getData());
-		//System.out.println(evento.getHorario());
-		//aqui foi importado o Evento que foi feito antes, pegou e mostrou no console o que foi digitado na pagina toda.//
-		//return "evento-test";
-	//}
 	@PostMapping("/eventos")
 	public String test(Evento evento) {
 		System.out.println(evento);
 		er.save(evento);
-		return "evento-test";
+		return "eventos/evento-test";
+		
 	}
 	
+	//github desktop//
 }
